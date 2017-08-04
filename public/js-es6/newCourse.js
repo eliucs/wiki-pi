27b68894c60e5ca467a66ee68e62e34423d94af7
Eric Liu <ericliu314@gmail.com>
2017-08-04 01:54:56 -0400

    Add newCourse.js and loading animation

---
+++ public/js-es6/newCourse.js
@@ -0,0 +1,102 @@
+/**
+* Client side JavaScript for new course page
+**/
+
+const errorFields = ["#empty-starting-article"];
+
+$("#text-similarity").mousemove(() => {
+  var percent = $("#text-similarity").val();
+  var text = percent + "%";
+  var color;
+
+  if (percent == 100) {
+    text += " Exactly the same"
+    color = "#1B5E20";
+  } else if (percent >= 90) {
+    text += " Significantly similar";
+    color = "#2E7D32";
+  } else if (percent >= 75) {
+    text += " Very similar";
+    color = "#388E3C";
+  } else if (percent >= 50) {
+    text += " Slightly similar";
+    color = "#43A047";
+  } else if (percent >= 25) {
+    text += " Slightly different";
+    color = "#E53935";
+  } else if (percent >= 10) {
+    text += " Very different";
+    color = "#D32F2F";
+  } else if (percent > 0) {
+    text += " Significantly different";
+    color = "#C62828";
+  } else if (percent == 0) {
+    text += " Exactly different";
+    color = "#B71C1C";
+  }
+
+  $("#text-similarity-percent").text(text);
+  $("#text-similarity-percent").css("color", color);
+});
+
+$("#btn-create-course").click(() => {
+  const startingArticle = typeof($("#starting-article").val()) == 'undefined' ?
+  "" : $("#starting-article").val();
+  const endingArticle = typeof($("#ending-article").val()) == 'undefined' ?
+  "" : $("#ending-article").val();
+  const textSimilarity = $("#text-similarity").val();
+
+  // For debugging:
+  // console.log(startingArticle);
+  // console.log(endingArticle);
+  // console.log(textSimilarity);
+
+  var errors = verifyFields(startingArticle);
+
+  if (errors.length > 0) {
+    errorFields.forEach((error) => {
+      $(error).css("display", "none");
+    });
+
+    errors.forEach((error) => {
+      $(error).css("display", "block");
+    });
+  } else {
+    $("#loading-container").css("display", "block");
+
+    errorFields.forEach((error) => {
+      $(error).css("display", "none");
+    });
+
+    var data = {
+      startingArticle: startingArticle,
+      endingArticle: endingArticle,
+      textSimilarity: textSimilarity
+    };
+
+    $.ajax({
+      type: 'POST',
+      data: JSON.stringify(data),
+      contentType: 'application/json',
+      url: '/new-course',
+      success: function(successCode) {
+        $("#loading-container").css("display", "none");
+        console.log(successCode);
+      },
+      error: function(errorCode) {
+        $("#loading-container").css("display", "none");
+        console.log(errorCode);
+      }
+    });
+  }
+});
+
+function verifyFields(startingArticle) {
+  var errors = [];
+
+  if (typeof(startingArticle) == 'undefined' || startingArticle.length == 0) {
+    errors.push("#empty-starting-article");
+  }
+
+  return errors;
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const _ = require('lodash');
 
 var app = express();
 
@@ -12,37 +14,47 @@ const staticPath = path.join(__dirname, '../public');
 app.use(express.static(staticPath));
 hbs.registerPartials(partialsPath);
 app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.render('index.hbs');
 });
 
-app.get('/about', function(req, res) {
+app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageName: "about",
     pageTitle: "About"
   });
 });
 
-app.get('/help', function(req, res) {
+app.get('/help', (req, res) => {
   res.render('help.hbs', {
     pageName: "help",
     pageTitle: "Help"
   });
 });
 
-app.get('/new-course', function(req, res) {
+app.get('/new-course', (req, res) => {
   res.render('new-course.hbs', {
     pageName: "new-course",
     pageTitle: "Create A New Course"
   });
 });
 
-app.get('/open-course', function(req, res) {
+app.post('/new-course', (req, res) => {
+  var body = _.pick(req.body, [
+    'startingArticle',
+    'endingArticle',
+    'textSimilarity'
+  ]);
+});
+
+app.get('/open-course', (req, res) => {
   res.render('open-course.hbs', {
     pageName: "open-course",
     pageTitle: "Open An Existing Course"
